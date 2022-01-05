59eeda968e01efb32ce4976c9888b2ae9adf5da7
christopherdevops <snake10547@outlook.com>
2022-01-05 15:27:51 +0800

    create next-app, set eslint, install mui

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import createEmotionCache from '../utility/createEmotionCache';
+import defaultThemeOptions from '../styles/themes/defaultThemeOptions';
+import '../styles/globals.css';
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const defaultTheme = createTheme(defaultThemeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
---
+++ styles/themes/defaultThemeOptions.tsx
@@ -0,0 +1,9 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+const defaultThemeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+  },
+};
+
+export default defaultThemeOptions;
